fb38b5dbdd1826fd74b07427c3ecbffc2d7b5944
Gibb Arko <gibbarko7@gmail.com>
2022-05-05 16:30:25 -0400

    Created testing project

---
+++ TechJobsTests/JobTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
+
+namespace TechJobsTests
+{
+    [TestClass]
+    public class JobTests
+    {
+        [TestMethod]
+        public void TestSettingJobId()
+        {
+            Job job1 = new Job();
+            Job job2 = new Job();
+
+            Assert.AreEqual(job1.Id, job2.Id - 1);
+            Assert.IsFalse(job1.Id == job2.Id);
+        }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+            Assert.AreEqual(job1.Name, "Product tester");
+            Assert.AreEqual(job1.EmployerLocation.Value, "Desert");
+            Assert.AreEqual(job1.JobType.Value, "Quality control");
+            Assert.AreEqual(job1.EmployerName.Value, "ACME");
+            Assert.AreEqual(job1.JobCoreCompetency.Value, "Persistence");
+        }
+
+        [TestMethod]
+        public void TestJobsForEquality()
+        {
+            Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Job job2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+            Assert.AreNotEqual(job1, job2);
+        }
+    }
+}
