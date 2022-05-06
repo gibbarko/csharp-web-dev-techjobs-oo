5545b089e04e7bb48237b84471ed56271276401a
Gibb Arko <gibbarko7@gmail.com>
2022-05-05 22:41:36 -0400

    finished coding Job ToString() and refactored re-used code into new JobField abstract class

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+
 namespace TechJobsOO
 {
     public class Job
@@ -40,6 +42,45 @@ namespace TechJobsOO
         public override int GetHashCode()
         {
             return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            if (this.Name == null && this.EmployerName == null && this.EmployerLocation == null && this.JobType == null && this.JobCoreCompetency == null)
+            {
+                return "OOPS! This job does not seem to exist.";
+            }
+
+            string stringId = this.Id.ToString();
+            string stringName = this.Name;
+            string stringEmployerName = this.EmployerName.ToString();
+            string stringEmployerLocation = this.EmployerLocation.ToString();
+            string stringJobType = this.JobType.ToString();
+            string stringJobCoreCompetency = this.JobCoreCompetency.ToString();
+
+            if (stringId == "") { stringId = "Data not available"; }
+            if (stringName == "") { stringName = "Data not available"; }
+            if (stringEmployerName == "") { stringEmployerName = "Data not available"; }
+            if (stringEmployerLocation == "") { stringEmployerLocation = "Data not available"; }
+            if (stringJobType == "") { stringJobType = "Data not available"; }
+            if (stringJobCoreCompetency == "") { stringJobCoreCompetency = "Data not available"; }
+
+            //string[] strings = { stringId, stringEmployerLocation,
+            //                     stringName, stringEmployerName,
+            //                     stringJobType, stringJobCoreCompetency };
+
+            //for (int i = 0; i < strings.Length; i++)
+            //{
+            //    if (strings[i] == "" || strings[i] == null) { strings[i] = "Data not available"; }
+            //}
+
+            return
+                $"\nID: {stringId}\n" +
+                $"Name: {stringName}\n" +
+                $"Employer: {stringEmployerName}\n" +
+                $"Location: {stringEmployerLocation}\n" +
+                $"Position Type: {stringJobType}\n" +
+                $"Core Competency: {stringJobCoreCompetency}\n";
         }
     }
 }
--- TechJobsTests/JobTests.cs
+++ TechJobsTests/JobTests.cs
@@ -37,5 +37,42 @@ namespace TechJobsTests
 
             Assert.AreNotEqual(job1, job2);
         }
+
+        [TestMethod]
+        public void TestJobToString()
+        {
+            Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            Job job2 = new Job();
+            Job job3 = new Job("Ice cream tester", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Tasting ability"));
+
+            string job1Name = job1.ToString();
+            string fullString1 = "\nID: 6\n" +
+                                "Name: Product tester\n" +
+                                "Employer: ACME\n" +
+                                "Location: Desert\n" +
+                                "Position Type: Quality control\n" +
+                                "Core Competency: Persistence\n";
+
+            string fullString2 = "\nID: 8\n" +
+                                "Name: Ice cream tester\n" +
+                                "Employer: Data not available\n" +
+                                "Location: Home\n" +
+                                "Position Type: UX\n" +
+                                "Core Competency: Tasting ability\n";
+
+            //Test that string begins and ends with a newline
+            Assert.AreEqual(job1Name[0], job1Name[job1Name.Length - 1]);
+            Assert.AreEqual(job1Name[0].ToString(), "\n");
+
+            //Test that string should contain a label for each field
+            Assert.AreEqual(fullString1, job1.ToString());
+
+            //Test data not available for empty strings
+            Assert.AreEqual(fullString2, job3.ToString());
+
+            //Test that entries with only an Id return “OOPS! This job does not seem to exist.”
+            Assert.AreEqual(("OOPS! This job does not seem to exist."), job2.ToString());
+
+        }
     }
 }
